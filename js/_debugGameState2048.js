20c8d215ca833df2bab684471e31b90f40f40a90
bdama <bdamay@gmail.com>
2023-04-20 19:55:44 +0200

    cheat state for debug

---
+++ js/_debugGameState2048.js
@@ -0,0 +1,3 @@
+var debug_game2048 =
+'{"grid":{"size":4,"cells":[[null,{"position":{"x":0,"y":1},"value":2},{"position":{"x":0,"y":2},"value":16},{"position":{"x":0,"y":3},"value":2048}],[null,null,{"position":{"x":1,"y":2},"value":8},{"position":{"x":1,"y":3},"value":4}],[null,null,{"position":{"x":2,"y":2},"value":4},{"position":{"x":2,"y":3},"value":2}],[null,null,null,{"position":{"x":3,"y":3},"value":2}]]},"score":20144,"over":false,"won":true,"keepPlaying":true}'
+
