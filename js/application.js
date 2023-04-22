5bac61460533a3df75fc119477847ebb1e57038b
Benoit Damay <bdamay@gmail.com>
2023-04-22 18:29:42 +0200

    Font size super revisited

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  // global game for debug purposes
+  game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
